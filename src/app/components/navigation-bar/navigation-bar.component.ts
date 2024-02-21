7f42febf6f47e1b1bf40b6589b5ca5e4f011779a
Lim Kent-Shen <kentshen_lim24@hotmail.com>
2024-02-21 16:33:26 +0800

    Fix navbar spacing

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -9,26 +9,20 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <nav
-      class="flex justify-between [&>div]:flex-grow [&>div]:text-center text-ACCENT [&_ion-icon]:text-lg bg-sky-200 pt-2"
+      class="flex justify-between [&>a]:flex-grow [&>a]:text-center text-ACCENT [&_ion-icon]:text-lg bg-sky-200 pt-2 [&>a]:navbar-centering"
     >
-      <div>
-        <a routerLink="/">
-          <ion-icon name="swap-horizontal-sharp"></ion-icon>
-          <p>Convert</p>
-        </a>
-      </div>
-      <div>
-        <a routerLink="/trend">
-          <ion-icon name="trending-up-sharp"></ion-icon>
-          <p>Trend</p>
-        </a>
-      </div>
-      <div>
-        <a routerLink="/news">
-          <ion-icon name="newspaper-sharp"></ion-icon>
-          <p>News</p>
-        </a>
-      </div>
+      <a routerLink="/">
+        <ion-icon name="swap-horizontal-sharp"></ion-icon>
+        <p>Convert</p>
+      </a>
+      <a routerLink="/trend">
+        <ion-icon name="trending-up-sharp"></ion-icon>
+        <p>Trend</p>
+      </a>
+      <a routerLink="/news">
+        <ion-icon name="newspaper-sharp"></ion-icon>
+        <p>News</p>
+      </a>
     </nav>
   `,
 })
