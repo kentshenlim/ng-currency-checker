199288db3e98b9a10df70c36dc64f9fa7162987e
Lim Kent-Shen <kentshen_lim24@hotmail.com>
2024-02-22 14:36:02 +0800

    Mark active navbar

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ Angular, [Freecurrency API](https://freecurrencyapi.com/), AG grid,
 Three key features: convertor, history, financial news.
 
 - Favicon
-- Change active color of tab in navbar
 - Last converted (local storage)
 - History (AG grid here), last 12 months
 - Last view history
--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,29 +1,43 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <nav
-      class="flex justify-between [&>a]:flex-grow [&>a]:text-center text-ACCENT [&_ion-icon]:text-lg bg-sky-200 pt-2 [&>a]:navbar-centering"
+      class="flex justify-between [&>a]:flex-grow [&>a]:text-center text-ACCENT [&_ion-icon]:text-lg bg-sky-200 [&>a]:navbar-centering [&>a]:pt-2"
     >
-      <a routerLink="/">
+      <a
+        routerLink="/"
+        routerLinkActive="nav-active-tab"
+        [routerLinkActiveOptions]="{ exact: true }"
+      >
         <ion-icon name="swap-horizontal-sharp"></ion-icon>
         <p>Convert</p>
       </a>
-      <a routerLink="/trend">
+      <a
+        routerLink="/trend"
+        routerLinkActive="nav-active-tab"
+        [routerLinkActiveOptions]="{ exact: true }"
+      >
         <ion-icon name="trending-up-sharp"></ion-icon>
         <p>Trend</p>
       </a>
-      <a routerLink="/news">
+      <a
+        routerLink="/news"
+        routerLinkActive="nav-active-tab"
+        [routerLinkActiveOptions]="{ exact: true }"
+      >
         <ion-icon name="newspaper-sharp"></ion-icon>
         <p>News</p>
       </a>
     </nav>
   `,
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent {
+  private activeStr: 'Convert' | 'Trend' | 'News' = 'Convert';
+}
