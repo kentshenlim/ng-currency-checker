7d9a7d125090d9981c5b06879fed771d4bbd95e8
Lim Kent-Shen <kentshen_lim24@hotmail.com>
2024-02-27 12:12:41 +0800

    Style error page

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -5,12 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="h-full flex flex-col items-center">
-      <h1 class="italic">Oops, the page you requested was not found...</h1>
-      <div
-        class="w-[70%] h-32 bg-center bg-no-repeat bg-contain opacity-80"
-        style="background-image: url('assets/images/broken-coin.png');"
-      ></div>
+    <div class="h-[70%] flex items-center justify-center">
+      <div class="flex flex-col items-center">
+        <div
+          class="w-[70%] h-32 bg-center bg-no-repeat bg-contain opacity-80"
+          style="background-image: url('assets/images/broken-coin.png');"
+        ></div>
+        <h1 class="italic md:text-lg mb-4 md:mb-8 text-center">
+          Oops, the page you requested was not found...
+        </h1>
+      </div>
     </div>
   `,
 })
